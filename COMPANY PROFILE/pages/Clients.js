dcd79f2e444563143a6c922371366f1d7c1c1ae3
heyitsrui <kervydelacruz789@gmail.com>
2026-01-28 17:53:49 +0800

    Add files via upload

---
+++ COMPANY PROFILE/pages/Clients.js
@@ -0,0 +1,7 @@
+const Clients = () => (
+  <div style={{ padding: '120px 5%' }}>
+    <h1>About Our Company</h1>
+    <p>Innovating the digital landscape since 2010.</p>
+  </div>
+);
+export default Clients;
